ee26f9996dcf974fcc2ef396d467290a55278eff
Max Lee <maxolee23@gmail.com>
2019-10-07 20:09:15 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,8 +2,19 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-  get '/name'
-    "My name is Max "
-  end
+  get '/' do
+      "Hello, World!"
+    end
 
+     get '/name' do
+      "My name is _"
+    end
+
+     get '/hometown' do
+      "My hometown is _"
+    end
+
+     get '/favorite-song' do
+      "My favorite song is _"
+    end
 end
